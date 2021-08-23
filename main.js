4cddb2d3bfe11f26321e054ea3a0cc54dcf5019a
Nano113 <37065523+Nano113@users.noreply.github.com>
2021-08-23 06:32:49 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+
+    let x= document.querySelector('#Login');
+    let y= document.querySelector('#Register')
+    let z= document.querySelector('#btn')
+
+    function Register(){
+    x.style.left="-400px"
+    y.style.left="50px"
+    z.style.left="110px"
+    }
+    
+    function Login(){
+    x.style.left="50px"
+    y.style.left="450px"
+    z.style.left="0px"
+    }
